88f96e5b0da000fe07002b6cfc2764b1b2f24083
Jimmy MUNOZ <jmunoz@adilibre.fr>
2021-07-18 09:06:16 +0200

    feat: add kaftka producer

---
+++ src/main/java/Producer.java
@@ -0,0 +1,30 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.*;
+
+import java.util.Properties;
+
+public class Producer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092,localhost:9093,localhost:9094");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("acks", "all");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+
+        int numOfRecords  = 100;
+        try {
+            Strign message = String.format("Producer %s has sent messa ")
+            for (int i = 0; i < numOfRecords; i++) {
+                System.out.println("Message " + i + " was just produced");
+                producer.send(new ProducerRecord<>("numbers", Integer.toString(i), Integer.toString(i)));
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        producer.close();
+    }
+}
